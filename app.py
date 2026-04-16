00463372d0f8024e3791490517b92a0cc75df78d
Fernando Odilon <fernando.odilon@aluno.senai.br>
2026-04-16 17:01:39 -0300

    MODIFY: Inicio da criação de tela de Cadastro, Login e Perfil do Usuário

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 from model.burguer import recuperar_produtos
+from model.usuario import cadastrar_usuario, recuperar_users
 app = Flask(__name__)
+
+app.secret_key = "MEGAMATS"
 
 @app.route('/')
 def index():
-    produtos = recuperar_produtos()
-    return render_template('./index.html',  produtos = produtos)
+    if session.get('usuario_logado'):    
+        produtos = recuperar_produtos()
+        return render_template('./index.html',  produtos = produtos)
+    return redirect('/login')
 
 @app.route('/produto/<codigo>')
 def produto(codigo):
@@ -13,6 +18,35 @@ def produto(codigo):
     print(produto)
     return render_template('./produto.html', produto = produto)
 
+@app.route('/cadastrar')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrar/cadastro', methods = ['POST'])
+def cadastrar_user():
+    usuario = request.form.get('nome')
+    senha = request.form.get('password')
+    senha_confirm = request.form.get('confirm-password')
+    print(usuario, senha, senha_confirm)
+    if cadastrar_usuario(usuario, senha) and len(senha) < 6 and senha != senha_confirm:
+        return redirect('/login')
+    return redirect('/cadastrar')
+
+@app.route('/login')
+def logar():
+    return render_template('login.html')
+
+@app.route('/logar', methods = ['POST'])
+def logar_user():
+    usuario = request.form.get('nome')
+    senha = request.form.get('password')
+    user = recuperar_users(usuario, senha)
+    if user:
+        session['usuario_logado'] = user
+        return render_template('perfil.html', user = user)
+    return redirect('/login')
+
+    
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ model/usuario.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from database.conexao import conectar
+
+def recuperar_users(usuario, password):
+    conexao, cursor = conectar()
+    cursor.execute('SELECT codigo, user_name, user_password FROM usuario WHERE user_name = %s AND user_password', (usuario, password))
+    user = cursor.fetchone()
+    if user:
+        return user
+    else:
+        return False
+    
+def cadastrar_usuario(usuario, password):
+    conexao, cursor = conectar()
+    cursor.execute('SELECT * FROM usuario;')
+    num_users = cursor.fetchall()
+    try:
+        cursor.execute('INSERT INTO usuario(codigo, user_name, user_password) VALUES(%s, %s, %s)', ((len(num_users) + 1), usuario, password))
+        conexao.commit()
+        conexao.close()
+        return True
+    except:
+        return False
+
+    
